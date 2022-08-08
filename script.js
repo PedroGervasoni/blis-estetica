a2c2544951dc8d20cc1d1e2040526e658f4d9423
PedroGervasoni <pedrogervasoni17@gmail.com>
2022-08-08 19:51:20 -0300

    retoques finales en contacto

--- script.js
+++ script.js
@@ -235,3 +235,5 @@ function miscPolyfillsForIE() {
   // https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent
   !function () {if ("function" == typeof window.CustomEvent) return !1;function t(t, e) {e = e || { bubbles: !1, cancelable: !1, detail: void 0 };var n = document.createEvent("CustomEvent");return n.initCustomEvent(t, e.bubbles, e.cancelable, e.detail), n;}t.prototype = window.Event.prototype, window.CustomEvent = t;}();
 }
+
+
